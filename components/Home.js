4125661d921d724a6b4f03f9ec35b3a6adfb97f6
Manu <emmanuel.ayad@gmail.com>
2024-05-02 13:25:42 +0200

    Composants Home Tweet Trends

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,46 @@
 import styles from '../styles/Home.module.css';
+import Image from 'next/image';
+import React from 'react';
+import Tweet from './Tweet';
+import Trends from './Trends';
 
 function Home() {
+
+
+
   return (
     <div>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+
+      <div className={styles.homeLeft}>
+      <span className={styles.logo}> <Image src='/logo.png' alt='logo-twitter' width={200} height={150}  /></span>
+
+          <div className= {styles.bottomHomeSection}>
+              <div className={styles.userInfo}>UserInfo</div>
+              <button className= {styles.logout} onClick={console.log('logout')}>Logout</button>
+          </div>
+      </div>
+
+
+
+      <div className={styles.tweetSection}>
+       
+            <Tweet/>
+
+                <div className={styles.lastTweetsSection}>
+                    <p>last tweets</p>
+                </div>
+          
+       
+
+        
+            
+      </div>
+
+      <div className={styles.trendsSection}> 
+        <Trends/>
+      </div>
+
       </main>
     </div>
   );
